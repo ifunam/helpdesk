be8a0500bacfb4146a958a747bb0797a633ddb44
carlos <carlos@carlos-laptop.(none)>
2009-11-23 14:33:49 -0600

    added ticket_version controller

---
+++ app/controllers/tech_support/ticket_versions_controller.rb
@@ -0,0 +1,9 @@
+class TechSupport::TicketVersionsController < ApplicationController
+  before_filter :user_profile
+
+  def show
+
+  end
+
+
+end
--- app/controllers/tech_support/versions_controller.rb
+++
@@ -1,6 +0,0 @@
-class TechSupport::VersionsController < ApplicationController
-  before_filter :user_profile
-  def index
-    @tickets = Ticket.search_and_paginate(params[:search], params[:page])
-  end
-end
