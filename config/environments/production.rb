03a49118b193ad43ef6034700135ee8707597785
Alejandro Juarez <alex@monsterlabs.com.mx>
2009-11-26 00:01:57 -0600

    Enabling Bundler for config/environments/*.rb and minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
+Bundler.require_env RAILS_ENV
 
 #1;2000;0c The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
+Bundler.require_env RAILS_ENV
 
 # The test environment is used exclusively to run your application's
 # test suite.  You never need to work with it otherwise.  Remember that
