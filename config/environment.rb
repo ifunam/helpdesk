a98b7f0563e7a0d75c3f601ea35de79fe9df7b10
Alejandro Juarez <alex@monsterlabs.com.mx>
2009-11-26 12:47:05 -0600

    Short config/environment.rb and config/mail.yml

--- config/environment.rb
+++ config/environment.rb
@@ -1,55 +1,14 @@
-
-# Be sure to restart your server when you modify this file
-
-# Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
-# Bootstrap the Rails environment, frameworks, and default configuration
+
 require File.join(File.dirname(__FILE__), 'boot')
-
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-
-  # Add additional load paths for your own custom dirs
-
-  config.load_paths += %W( #{RAILS_ROOT}/clients )
-  # config.action_controller.relative_url_root = "/var/www/help_desk/public"
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-  # config.gem 'erector', :lib => 'erector'
  
-  # Only load the plugins named here, install the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
-
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
+  config.load_paths += %W( #{RAILS_ROOT}/clients )
+  config.time_zone = 'Mexico City'
   config.active_record.observers = :ticket_observer, :comment_observer if RAILS_ENV != 'test'
 
   config.action_mailer.register_template_extension('haml')
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-    :address => 'fenix.fisica.unam.mx',
-    :port => 25,
-    :domain => 'fisica.unam.mx',
-    :authentication => :plain,
-    :location => '/usr/sbin/sendmail',
-    :arguments => '-i -t'
-  }
+  sendmail_config = YAML.load_file(RAILS_ROOT + "/config/mail.yml")
+  config.action_mailer.sendmail_settings = sendmail_config['settings']
 end
